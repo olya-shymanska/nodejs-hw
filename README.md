7b8e5964dbb0bcd2c3da3872fd6cabe4d0a0582c
olya shymanska <olyashymanska3@gmail.com>
2026-03-01 17:46:48 -0600

    task finished

---
+++ README.md
@@ -0,0 +1,1 @@
+Read me document for this homework
---
+++ src/server.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import pino from 'pino-http';
+
+const app = express();
+
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: "Retrieved all notes" });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+
+  console.error("Error:", err.message);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd ?
+      'Internal Server Error' :
+    err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
+
+
