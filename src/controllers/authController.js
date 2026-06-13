8704301a10289281114ef1f5933807ef282191cc
olya shymanska <olyashymanska3@gmail.com>
2026-06-13 15:04:10 -0500

    request reset done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,11 @@ import { setSessionCookies } from '../services/auth.js';
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 import { Session } from '../models/session.js';
+import jwt from 'jsonwebtoken';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import handlebars from 'handlebars';
+import { sendEmail } from '../utils/sendEmail.js';
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -88,5 +93,43 @@ export const logoutUser = async (req, res) => {
 };
 
 export const requestResetEmail = async (req, res) => {
+  const { email } = req.body;
 
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(200).json({message: 'Password reset email sent successfully'});
+  };
+
+  const resetToken = jwt.sign(
+    { sub: user._id, email },
+    process.env.JWT_SECRET,
+    {expiresIn: '15m' },
+  );
+
+  const templatePath = path.resolve('src/templates/reset-password-email.html');
+  const templateSource = await fs.readFile(templatePath, 'utf-8');
+
+  const template = handlebars.compile(templateSource);
+
+  const html = template({
+    name: user.username,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
+  });
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: 'Reset your password',
+      html,
+    });
+  }
+  catch {
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
+  };
+  res.status(200).json({ message: 'Password reset email sent successfully' });
 };
