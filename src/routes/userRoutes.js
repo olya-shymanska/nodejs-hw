621caf62bbe0011491758954ec1e648fb68b07bd
olya shymanska <olyashymanska3@gmail.com>
2026-06-26 15:35:18 -0500

    done

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/authenticate.js";
 import { updateUserAvatar } from "../controllers/userControllers.js";
+import { upload } from "../middleware/multer.js";
 
 const router = Router();
 
-router.patch('/users/me/avatar', authenticate, updateUserAvatar);
+router.patch('/users/me/avatar',
+  authenticate,
+  upload.single('avatar'),
+  updateUserAvatar);
 
 export default router;
